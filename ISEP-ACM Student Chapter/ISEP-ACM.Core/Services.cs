a5243982291215503882c51e81feda8f16a134d3
André Silva <2493377+askpt@users.noreply.github.com>
2013-12-23 14:53:03 +0000

    Added the http services for the application

---
+++ ISEP-ACM Student Chapter/ISEP-ACM.Core/Services.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISEP_ACM.Core
+{
+    public class Services
+    {
+        public static async Task<Posts> GetAllPosts()
+        {
+            HttpClient client = new HttpClient();
+
+            var baseUrl = "http://193.136.60.87/portal/?json=get_posts";
+
+            string jsonPosts = await client.GetStringAsync(baseUrl);
+
+            Posts posts = JsonConvert.DeserializeObject<Posts>(jsonPosts);      
+
+            return posts;
+        
+        }
+
+    }
+}
