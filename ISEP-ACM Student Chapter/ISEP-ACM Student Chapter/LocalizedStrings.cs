65edc5167d13885e1cbe269310906a2d78d19785
André Silva <2493377+askpt@users.noreply.github.com>
2013-12-23 14:32:17 +0000

    Created a new WP project and modified the main page.

---
+++ ISEP-ACM Student Chapter/ISEP-ACM Student Chapter/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using ISEP_ACM_Student_Chapter.Resources;
+
+namespace ISEP_ACM_Student_Chapter
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
