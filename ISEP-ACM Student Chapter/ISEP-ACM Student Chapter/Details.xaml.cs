43b013da94e6e9a32ab69e962a58535cc3b2e345
André Silva <2493377+askpt@users.noreply.github.com>
2013-12-23 17:45:29 +0000

    Added the content view to the details page

--- ISEP-ACM Student Chapter/ISEP-ACM Student Chapter/Details.xaml.cs
+++ ISEP-ACM Student Chapter/ISEP-ACM Student Chapter/Details.xaml.cs
@@ -7,6 +7,8 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using ISEP_ACM_Student_Chapter.Resources;
+using ISEP_ACM.Core;
 
 namespace ISEP_ACM_Student_Chapter
 {
@@ -15,6 +17,47 @@ namespace ISEP_ACM_Student_Chapter
         public Details()
         {
             InitializeComponent();
+
+            BuildLocalizedApplicationBar();
+                        
+        }
+
+        // Sample code for building a localized ApplicationBar
+        private void BuildLocalizedApplicationBar()
+        {
+            // Set the page's ApplicationBar to a new instance of ApplicationBar.
+            ApplicationBar = new ApplicationBar();
+
+            // Create a new button and set the text value to the localized string from AppResources.
+            ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+            appBarButton.Text = AppResources.AppBarButtonText;
+            appBarButton.Click += appBarButton_Click;
+            ApplicationBar.Buttons.Add(appBarButton);
+        }
+
+        private void appBarButton_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            int index = Convert.ToInt32(NavigationContext.QueryString["id"]);
+
+            Posts posts = await Services.LoadPosts();
+            foreach (Post item in posts.posts)
+            {
+                if (item.id == index)
+                {
+                    DataContext = item;
+
+                    BrowserView.NavigateToString(WebBrowserHelper.WrapHtml(item.content, BrowserView.ActualWidth)); 
+                }
+            }
+
+            
+
         }
     }
 }
