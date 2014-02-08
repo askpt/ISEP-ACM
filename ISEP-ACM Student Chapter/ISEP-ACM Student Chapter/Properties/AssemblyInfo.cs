d0b93c71d7f62c5f4207e74343f3738907b9aed9
André Silva <2493377+askpt@users.noreply.github.com>
2014-02-08 17:17:33 +0000

    Updated Assembly infos

--- ISEP-ACM Student Chapter/ISEP-ACM Student Chapter/Properties/AssemblyInfo.cs
+++ ISEP-ACM Student Chapter/ISEP-ACM Student Chapter/Properties/AssemblyInfo.cs
@@ -6,13 +6,13 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ISEP_ACM_Student_Chapter")]
+[assembly: AssemblyTitle("ISEP-ACM Student Chapter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ISEP_ACM_Student_Chapter")]
+[assembly: AssemblyProduct("ISEP-ACM Student Chapter")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("askpt")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -34,4 +34,4 @@ using System.Resources;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("en-US")]
+[assembly: NeutralResourcesLanguageAttribute("pt")]
